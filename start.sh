b39f654e57f6d0e9ffba46a3cb56e58c4a3ddfc2
juan guillermo resendiz rojas <resendizjg@globalhitss.com>
2025-11-28 10:10:37 -0600

    apprunner.yaml

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Instalando dependencias..."
-python3 -m pip install --user -r requirements.txt
-
-echo "Iniciando Streamlit..."
-python3 -m streamlit run streamlit.py --server.port=8080 --server.address=0.0.0.0 --server.headless=true
